1175c5aa6bdd65a2a4abcc8d4fb147f529d5a5ea
littleknife <842305768@qq.com>
2024-11-17 19:55:42 +0800

    safe html implemented by StringLiteral&StringInterpolation

--- src/utility/Sources/main.swift
+++ src/utility/Sources/main.swift
@@ -1,9 +1,87 @@
 ﻿// Console app.
 
+import Foundation
+
+extension String {
+    var htmlEscaped:String {
+        return replacingOccurrences(of: "<", with: "&lt;").replacingOccurrences(of: ">", with: "&gt;")
+    }
+}
+
+struct SafeHTML {
+    private(set) var value:String
+    init(unsafe html:String){
+        self.value = html.htmlEscaped
+    }
+}
+
+extension SafeHTML:ExpressibleByStringLiteral {
+    public init(stringLiteral value:StringLiteralType){
+        self.value = value
+    }
+}
+
+extension SafeHTML:ExpressibleByStringInterpolation {
+   init(stringInterpolation: SafeHTML) {
+      self.value = stringInterpolation.value
+    }
+}
+
+extension SafeHTML:StringInterpolationProtocol {
+    init(literalCapacity: Int, interpolationCount: Int) {
+        self.value = ""
+        value.reserveCapacity(literalCapacity)
+    }
+
+    mutating func appendLiteral(_ literal: StringLiteralType) {
+        value += literal
+    }
+
+    mutating func appendInterpolation<T>(_ x:T) {
+        self.value += String(describing: x).htmlEscaped
+    }
+
+    mutating func appendInterpolation<T>(raw x:T) {
+        self.value += String(describing: x)
+    }
+}
+
+
+
+
 func main() {
 
-    print("Hello world");
+//    let safe:SafeHTML = "<p>Angle brackets in literals are not escaped</p>"
+
+    let unsafeInput = "<script>alert('Oops!')</script>"
+    let safe:SafeHTML = "<li>Username:\(unsafeInput)</li>"
+    print(safe)
+
+    let star = "<sup>*</sup>"
+    let safe2:SafeHTML = "<li>Username\(raw: star):\(unsafeInput)</li>"
+    print(safe2)
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 main()
