023f45c003d2763ec503fcf29c4dbcf522519175
littleknife <842305768@qq.com>
2024-11-20 21:50:27 +0800

    swift generic type programming snippet

--- src/utility/Sources/main.swift
+++ src/utility/Sources/main.swift
@@ -46,8 +46,34 @@ extension SafeHTML:StringInterpolationProtocol {
     }
 }
 
+extension SafeHTML:CustomStringConvertible {
+    var description: String {
+        return self.value
+    }
+}
+
+extension SafeHTML:CustomDebugStringConvertible {
+    var debugDescription: String {
+        return "SafeHTML:\(value)"
+    }
+}
+
+//打开全模块优化
+//将泛型函数标记为@inline 以便其他模块使用
+//标准库大量使用@inlinable来确保API的特化
+//编译器标记会把模块中的所有API标记为可内联来尝试这一过程自动化
+//C++ Rust 所有东西进行泛型特化
+//Java 只把泛型作为类型检查，在运行时通过包装将它抹去
+//Swift 允许对泛型函数或类型在声明侧和客户端的使用进行分别编译
+//它允许Apple在它的SDK中以二进制的方式搭载Swift书写的框架
+//类似的，在我们自己的代码中，也可以用泛
+//型抽象任何与类型无关的逻辑细节，并以此划分出更加清晰的责任边
+//界。
 
 
+@inlinable func min<T:Comparable>(_ x:T, _ y:T) -> T {
+    return y < x ? y : x
+}
 
 func main() {
 
@@ -60,6 +86,9 @@ func main() {
     let star = "<sup>*</sup>"
     let safe2:SafeHTML = "<li>Username\(raw: star):\(unsafeInput)</li>"
     print(safe2)
+
+    let res = min(11.2, 12.2)
+    print("\(res)")
 
 }
 
